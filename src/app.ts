b72f8ac3bf5d4db46f1bc2234d6af6ad46d085c2
Oleh Shch <oleh.shchevrovskyi@gmail.com>
2023-04-30 12:12:16 +0200

    set user's location

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
+interface userLocation {
+  ip: string;
+  city: string;
+  country: string;
+  latitude: number;
+  longitude: number;
+}
+
 class Widget {
   private widgetContainer: HTMLElement;
   private btnConteiner: HTMLElement;
-  private userIP: string = "";
+  private userLocation: userLocation | undefined;
+
+  set newLocation(locationObj: userLocation) {
+    this.userLocation = locationObj;
+  }
+
+  get locationData() {
+    return this.userLocation;
+  }
 
   constructor() {
     this.widgetContainer = document.createElement("article");
@@ -16,6 +32,7 @@ class Widget {
   }
 
   initializeBtnSection() {
+    this.btnConteiner.innerHTML = "";
     const locationEl = document.createElement("span");
     locationEl.classList.add("location");
 
@@ -24,17 +41,39 @@ class Widget {
     this.btnConteiner.append(locationEl);
     this.btnConteiner.append(closeBtn);
 
-    if (this.userIP === "") {
+    if (this.userLocation) {
+      console.log(this.userLocation);
+      const { city, country, ip } = this.locationData!;
+      locationEl.textContent = city + ", " + country;
+      locationEl.title = ip;
     }
   }
 
   private mountWidget() {
     document.body.append(this.widgetContainer);
+    const styleEl = document.createElement("style");
+    styleEl.innerHTML =
+      "@import url('https://fonts.googleapis.com/css2?family=Dosis:wght@300;400;600&display=swap');";
+    document.body.prepend(styleEl);
   }
 
   private setLoading() {
     this.widgetContainer.classList.add("loading");
   }
+
+  async fetchIP() {
+    const responce = await fetch("https://ipapi.co/json/");
+    const data = await responce.json();
+    const { ip, city, country, latitude, longitude } = data;
+    this.userLocation = { ip, city, country, latitude, longitude };
+  }
 }
 
-const app = new Widget();
+const app = async () => {
+  const widget = new Widget();
+
+  await widget.fetchIP();
+  widget.initializeBtnSection();
+};
+
+app();
